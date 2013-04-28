59f31e41a86c2a9971ee8479c114e6d7ae40290b
Rain <rainelemental@gmail.com>
2013-04-28 13:00:17 -0400

    update db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,22 @@
 <?php
-	$db['default']['dbserver'] = 'mysql';
-	$db['default']['hostname'] = 'localhost';
-	$db['default']['username'] = 'root';
-	$db['default']['password'] = 'root';
-	$db['default']['database'] = 'benchmark';
-/*
-To override db connection details, copy the above config values into a file called config.db.local.php,
-which is set to be ignored by git and thus will not be committed inadvertently!
-*/
-if (file_exists(dirname(__FILE__).DIRECTORY_SEPARATOR.'config.db.local.php')) {
-	include dirname(__FILE__).DIRECTORY_SEPARATOR.'config.db.local.php';
-}
-?>
+
+$db = array(
+    // development database (default)
+    'dev' => array(
+        'driver' => 'mysql',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'root',
+        'database' => 'benchmark'
+    ),
+    //production database (live website)
+    'prod' => array(
+        'driver' => '',
+        'hostname' => '',
+        'username' => '',
+        'password' => '',
+        'database' => ''
+    )
+);
+
+// -- end
